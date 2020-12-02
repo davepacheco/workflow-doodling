82ed493f3f504c4e3c3e82efc3ad2532869251ed
David Pacheco <dap@oxidecomputer.com>
2020-12-02 13:02:14 -0800

    add basic action trait and example

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
+use core::future::Future;
+
 type WfError = anyhow::Error;
 type WfResult<T> = Result<T, WfError>;
 
-trait WfAction<I, O> {
-    fn do_it(&self, input: I) -> WfResult<O>;
+trait WfAction<I, O, Fut>
+where
+    Fut: Future<Output=WfResult<O>>
+{
+    fn do_it(&self, input: I) -> Fut;
 }
 
-impl<I, O, F> WfAction<I, O> for F
+impl<I, O, F, Fut> WfAction<I, O, Fut> for F
 where
-    F: Fn(I) -> WfResult<O>
+    F: Fn(I) -> Fut,
+    Fut: Future<Output=WfResult<O>>
 {
-    fn do_it(&self, input: I) -> WfResult<O> {
+    fn do_it(&self, input: I) -> Fut {
         (self)(input)
     }
 }
@@ -18,9 +24,21 @@ where
 mod test {
     use petgraph::graph::Graph;
     use petgraph::visit::Topo;
+    use super::WfAction;
+    use super::WfResult;
+
+    async fn do_thing_1(label: &str) -> WfResult<usize> {
+        Ok(label.len())
+    }
+
+    #[tokio::test]
+    async fn demo_wf_action() {
+        let w: &dyn WfAction<&str, usize, _> = &do_thing_1;
+        eprintln!("dap: {}", w.do_it("hello").await.unwrap());
+    }
 
     #[test]
-    pub fn demo() {
+    fn demo() {
         /*
          * Create a 5-node graph that looks like this:
          *
@@ -45,7 +63,7 @@ mod test {
         g.add_edge(na, nb, ());
         g.add_edge(na, nc, ());
         g.add_edge(na, nd, ());
-    
+
         let mut topo = Topo::new(&g);
         while let Some(n) = topo.next(&g) {
             eprintln!("visit: {}", g[n]);
