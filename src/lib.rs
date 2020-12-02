5ddd8506270cdd41bd5332b74a3a957b2602d28d
David Pacheco <dap@oxidecomputer.com>
2020-12-02 15:40:52 -0800

    document where I am so far

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,133 @@
+/*
+ * This file contains an in-progress prototype interface for workflows.
+ *
+ * Fundamentally, workflows are a directed acyclic graph of possibly reversible
+ * actions, similar to distributed sagas.  There are two main functional pieces:
+ * an interface for constructing the graph and an execution engine for carrying
+ * out the actions described by the graph.  There are many subparts to this
+ * problem, including:
+ *
+ * - What's the syntax for constructing the graph?  Can graphs be modified while
+ *   they're being executed?  (If not, the syntax for constructing them can
+ *   probably be made a lot cleaner.  But some workflows may require this, as
+ *   when step N is used to stamp out a bunch of different instances of step N +
+ *   1.)
+ *
+ *   To keep things simple for now, we're going to make the construction totally
+ *   explicit.
+ *
+ * - How is data shared between different actions in the graph?  Ideally, this
+ *   would be statically type-checked, so that you could not add a node B to the
+ *   graph that uses data that wasn't provided by some ancestor of B in the
+ *   graph.  It's not clear how to express this in Rust without a lot of
+ *   boilerplate or macros.
+ *
+ *   We're deferring this for now.  _Right_ now, there's no built-in way to
+ *   share any data at all.  The next step will be to add some generic state
+ *   parameter provided to every function.
+ *
+ *   In terms of modifying this state, we'll need to consider whether we want to
+ *   use Mutexes to protect them or provide a single execution thread/task with
+ *   messages sent on a channel to update state.
+ *
+ * - How do execution parameters like canarying, limited concurrency, and
+ *   limiting blast radius fit in?
+ *
+ * - Persistence: what state do we persist and how?  More on this below
+ *
+ * The current status is that we have:
+ *
+ * - basic types: WfError, WfResult
+ * - WfAction, which has a blanket impl for functions to minimize boilerplate
+ * - WfBuilder, an interface for constructing a workflow graph pretty much by
+ *   hand
+ *
+ * At this point, we can concisely express a set of steps that make up a
+ * workflow.  Remaining things to de-risk:
+ *
+ * - execution: build an executor that walks the graph and executes exactly the
+ *   steps that it's allowed to execute with maximum parallelism
+ * - shared data: flesh out the demo implementation by having the functions
+ *   actually share data
+ * - composeability: is it possible given what we've done at this point to
+ *   insert an entire Workflow graph in the middle of another workflow?  It
+ *   seems like the big problem here is that the state objects will differ.
+ *   Maybe we impl WfAction for WfBuilder, and require that for that to work
+ *   there must be an Into<...> to map one state type to the other?  Is that
+ *   even possible?  (If you modify the state, do you need to be able to map it
+ *   back?)
+ * - persistence!  Can we get away with persisting only what nodes have started
+ *   and finished?  Derisking this will involve not just persisting the state
+ *   but making sure we can reconstitute the state from the persisted form and
+ *   correctly execute the workflow.
+ * - Reversibility: how do we allow consumers to express that some or all of
+ *   these actions are reversible and then implement that such that if a
+ *   Workflow step fails in a way that demands reversal of the workflow, then we
+ *   carry that out?  And how do we determine whether errors within subworkflows
+ *   demand that the parent workflow also be reversed?
+ * - bells and whistles:
+ *   - concurrency limit
+ *   - canarying
+ *   - blast radius?
+ * - revisit static typing in the construction and execution of the graph?  We
+ *   could say that each node has its own input type and maybe use a macro to
+ *   make that ergonomic (so that people don't have to define a new type for
+ *   every stage).  But what about the way they save state?  Or maybe
+ *   equivalently: how do we transform the output type from one phase into the
+ *   input type for the next?
+ *
+ * Persistence is a big question.  Can we get away with persisting _only_ what
+ * distributed sagas does: namely, which steps we've started and finished?  As a
+ * simple example of additional state we might like to store, consider the IP
+ * address of an Instance on its VPC.  Formally, for distributed sagas, we'd
+ * want to say that `vpc_allocate_ip` is idempotent and we would simply have the
+ * executor run this node again in the event of a crash.  For that to work, we'd
+ * want it to find the previously-allocated IP.  If all we persisted was which
+ * steps we'd run already, then in order to idempotently implement this, we'd
+ * presumably need `vpc_allocate_ip` to record not just that the IP was
+ * allocated, but which workflow allocated it, and then the idempotent
+ * implementation would look for IPs allocated by this workflow.  That's fine, I
+ * guess, but it seems weird for the VIP database records to include information
+ * about the workflow system that they otherwise shouldn't really know much
+ * about.  On the other hand, maybe the VIP is linked to the _instance_.  In
+ * that case, we need the Instance id available to the workflow (so that we can
+ * search for existing VIPs allocated for this Instance), which brings us to the
+ * same problem: do Instances know what Workflow ID created them (shudder), or
+ * does the Workflow have a way to persist the Instance id once it's been
+ * assigned?  On the other hand, if Instances _don't_ know what workflow created
+ * them, then how could one possibly implement the instance_create() workflow
+ * step idempotently?  Maybe we split this into two steps:
+ * instance_id_allocate() followed by instance_create()?  This way if we crash
+ * we'll only ever try to recreate an Instance with the same ID (or,
+ * equivalently, we can look it up first).  But this still assumes that the
+ * instance_id_allocate() step is able to save its output somewhere.
+ * (Alternatively, in this specific case, we could say that the instance id is
+ * part of the immutable parameters to the Workflow that's persisted when we
+ * create the workflow.  Does that always work?)
+ *
+ * What it boils down to is: it _feels_ a lot easier to allow stages to persist
+ * whatever state they want; however, this also feels very easy to implement
+ * incorrectly.  Imagine you haven't considered the above problem and you do the
+ * obvious thing of having instance_create() allocate an id, construct a new
+ * record, save it to the database, and then record persistent workflow state
+ * consisting solely of the instance id.  If you fail after the database
+ * operation, you'll run this again, create a new id and Instance, and leak the
+ * Instance row in the database.
+ *
+ * Maybe another idea is that you can save persistent state only in synchronous
+ * actions.  This would prevent you from implementing the above -- you'd _have_
+ * to separate it into a synchronous step (allocating the id) that saves
+ * persistent state and an asynchronous state (that uses the id to create the
+ * instance).
+ *
+ * Another thing to consider: do we expect an Instance to be operated on
+ * multiple workflows in its lifetime?  (Probably -- create, destroy, maybe even
+ * boot and halt.)  What about concurrently?  (It seems likely.)  Do we want
+ * every row in the Instance table to have a column for every possible workflow?
+ * That kind of sucks.  (On the other hand, it might help us avoid issuing
+ * multiple concurrent reboot workflows or something like that.)
+ */
+
 #![feature(type_name_of_val)]
 
 use core::fmt;
@@ -9,6 +139,7 @@ use petgraph::graph::NodeIndex;
 #[macro_use]
 extern crate async_trait;
 
+/* Widely-used types (within workflows) */
 type WfError = anyhow::Error;
 type WfResult = Result<(), WfError>;
 
@@ -90,6 +221,12 @@ async fn demo_prov_server_pick() -> WfResult {
     eprintln!("pick server");
     Ok(())
 }
+/*
+ * TODO: the interface we've created so far does not support the two-step
+ * operation consisting of "pick server" followed by "allocate server
+ * resources".  A solution to composeability might address this (see above)
+ * because this could be a subworkflow.
+ */
 async fn demo_prov_server_reserve() -> WfResult {
     eprintln!("reserve server");
     Ok(())
@@ -111,8 +248,17 @@ async fn demo_prov_instance_boot() -> WfResult {
     Ok(())
 }
 
+/*
+ * WfBuilder is an interface for constructing a workflow graph.  See `append()`
+ * and `append_parallel()` for more.
+ */
 #[derive(Debug)]
-struct WfBuilder {
+pub struct WfBuilder {
+    /*
+     * DAG of workflow nodes.  We use the WfAction itself as the "weight".  It's
+     * not clear if it's intended to be used this way.  If not, we could replace
+     * this with a BTreeMap of node indexes to Box<dyn WfAction>.
+     */
     graph: Graph<Box<dyn WfAction>, ()>,
     root: NodeIndex,
     last: Vec<NodeIndex>,
@@ -135,6 +281,11 @@ impl WfBuilder {
         }
     }
 
+    /*
+     * Creates a new "phase" of the workflow consisting of a single action.
+     * This action will depend on completion of all actions in the previous
+     * phase.
+     */
     fn append(&mut self, action: Box<dyn WfAction>) {
         let newnode = self.graph.add_node(action);
         for node in &self.last {
@@ -144,6 +295,11 @@ impl WfBuilder {
         self.last = vec![newnode];
     }
 
+    /*
+     * Creates a new "phase" of the workflow consisting of a set of actions that
+     * may run concurrently.  These actions will individually depend on all
+     * actions in the previous phase having been completed.
+     */
     fn append_parallel(&mut self, actions: Vec<Box<dyn WfAction>>) {
         let newnodes: Vec<NodeIndex> =
             actions.into_iter().map(|a| self.graph.add_node(a)).collect();
@@ -172,15 +328,21 @@ impl WfBuilder {
     }
 }
 
-fn make_provision_workflow() -> WfBuilder {
+/*
+ * Construct a demo "provision" workflow matching the description above.
+ */
+pub fn make_provision_workflow() -> WfBuilder {
     let mut w = WfBuilder::new();
 
     w.append(Box::new(demo_prov_instance_create));
     w.append_parallel(vec![
         Box::new(demo_prov_vpc_alloc_ip),
         Box::new(demo_prov_volume_create),
+        Box::new(demo_prov_server_pick),
     ]);
     w.append(Box::new(demo_prov_instance_configure));
+    w.append(Box::new(demo_prov_volume_attach));
+    w.append(Box::new(demo_prov_instance_boot));
 
     w
 }
@@ -188,10 +350,12 @@ fn make_provision_workflow() -> WfBuilder {
 #[cfg(test)]
 mod test {
     use super::make_provision_workflow;
-    use super::WfAction;
-    use super::WfBuilder;
-    use super::WfResult;
 
+    /*
+     * Exercises much of the code here by constructing the demo provision
+     * workflow.  We print the "Dot"-format graph to stderr so that we can
+     * visually inspect the result.
+     */
     #[test]
     fn test_make_provision() {
         let w = make_provision_workflow();
